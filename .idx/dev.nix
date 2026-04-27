47ef9cbe7527150ad6a791b9801622cbcac2f5b2
cghi1260229 <cghi1260229@gn.iwasaki.ac.jp>
2026-04-27 07:09:25 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,7 +4,7 @@
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  packages = [ pkgs.python3, pkgs.sqlite ];
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [ "ms-python.python" ];
@@ -14,7 +14,7 @@
         install =
           "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "README.md" "src/index.html" "main.py" ];
+        default.openFiles = [ "README.md" "templates/index.html" "main.py" ];
       }; # To run something each time the workspace is (re)started, use the `onStart` hook
     };
     # Enable previews and customize configuration
